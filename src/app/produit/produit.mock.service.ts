529bcb27c62a7e2d58295a4c6c42f7745a0bf566
Lahcen Naoui <lahcen.naoui@gmail.com>
2019-04-29 12:27:43 +0100

    Création de component, model et mock-service de produits

---
+++ src/app/produit/produit.mock.service.ts
@@ -0,0 +1,19 @@
+import {Produit} from '../shared/produit';
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class ProduitMockService{
+
+    private PRODUITS: Produit[] = [];
+    constructor(){
+        let p1: Produit = new Produit('livre',50,20);
+        let p2: Produit = new Produit('Cahier',5,25);
+        let p3: Produit = new Produit('Stylo',2,10);
+        this.PRODUITS.push(p1);
+        this.PRODUITS.push(p2);
+        this.PRODUITS.push(p3);
+    }
+    public getProduits(): Produit[]{
+        return this.PRODUITS;
+    }
+}
---
+++ src/app/shared/produit.ts
@@ -0,0 +1,7 @@
+export class Produit{
+    constructor(public ref?: String,
+        public quantite?: number,
+        public prixunitaire?: number){
+
+    }
+}
